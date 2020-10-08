1487ed66f360074035e1f362eea1a3498bca60b7
seiha-bank <68206645+seiha-bank@users.noreply.github.com>
2020-10-07 22:48:29 -0400

    Update config.js
    Parking Filter

--- config.js
+++ config.js
@@ -2,18 +2,20 @@ const config = {
   style: 'mapbox://styles/seiha-men/ckcv43tkn03ld1irxp9uqghpw',
   accessToken: 'pk.eyJ1Ijoic2VpaGEtbWVuIiwiYSI6ImNrYWhleDBsaDAxM2UyeG80OHc1emp6bXIifQ.R0aJpShr0KmWvJDcEYab5Q',
   CSV: 'https://docs.google.com/spreadsheets/d/1GLWqvoaBXHYIPWJZ52Co8MuFRMtdIsvU9BZZuLVsmH0/gviz/tq?tqx=out:csv&sheet=Sheet1',
-  center: [-75.701, 45.421], // Lng, Lat
-  zoom: 15, // Default zoom
+  center: [-75.697, 45.416], // Lng, Lat
+  zoom: 14, // Default zoom
   title: 'Go Bank Businesses',
   description: 'Explore our wonderful shops and enjoy what the neighborhood has to offer.',
   sideBarInfo: ['Name', 'Address', 'Hours'],
   popupInfo: ['Name'],
+  popupDescription: ['Website'],
   filters: [
     {
       type: 'dropdown',
       title: 'Type of Business: ',
       columnHeader: 'Type',
       listItems: [
+        'Parking',
         'Financial Institution',
         'Coffee & Tea',
         'Beauty & Wellness',
@@ -25,8 +27,9 @@ const config = {
         'Food & Drink',
         'Food',
         'Services'
-      ]
+      ] 
     }
+
   ]
 
 };
