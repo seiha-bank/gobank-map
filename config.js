66609009cea180fa20eaed495ce19cacf393114c
seiha-bank <68206645+seiha-bank@users.noreply.github.com>
2020-10-22 20:53:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  style: 'mapbox://styles/seiha-men/ckcv43tkn03ld1irxp9uqghpw',
+  style: 'mapbox://styles/seiha-men/ckgbl2xcb29qc19s0ltig7ux0',
   accessToken: 'pk.eyJ1Ijoic2VpaGEtbWVuIiwiYSI6ImNrYWhleDBsaDAxM2UyeG80OHc1emp6bXIifQ.R0aJpShr0KmWvJDcEYab5Q',
   CSV: 'https://docs.google.com/spreadsheets/d/1GLWqvoaBXHYIPWJZ52Co8MuFRMtdIsvU9BZZuLVsmH0/gviz/tq?tqx=out:csv&sheet=Sheet1',
   center: [-75.697, 45.416], // Lng, Lat
@@ -8,7 +8,8 @@ const config = {
   description: 'Explore our wonderful shops and enjoy what the neighborhood has to offer.',
   sideBarInfo: ['Name', 'Address', 'Hours'],
   popupInfo: ['Name'],
-  popupDescription: ['Phone'],
+  popupContact: ['Phone'],
+  popupDescription: ['Website'],
   filters: [
     {
       type: 'dropdown',
